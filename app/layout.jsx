4810dab20dca05380347089038b282dd80dfd730
Ali Shahid <ali@MacBookAir.lan>
2025-07-17 18:28:27 -0400

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import "@/assets/styles/globals.css";
+export const metadata = {
+  title: "Property Pulse",
+  keywords: "rental, property, real estate",
+  description: "Find the perfect rental property",
+};
+function layout({ children }) {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
+
+export default layout;
---
+++ app/page.js
@@ -0,0 +1,5 @@
+function page() {
+  return <div className="bg-red-200 text-2xl">Home page</div>;
+}
+
+export default page;
